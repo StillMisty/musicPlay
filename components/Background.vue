4adf91ec0cb397cbe3d09a4df738d0b51f2c9492
still <1772797842@qq.com>
2025-03-02 23:08:21 +0800

    添加切换动画

--- components/Background.vue
+++ components/Background.vue
@@ -1,14 +1,37 @@
 <template>
-  <video
-    autoplay
-    muted
-    loop
-    class="absolute left-0 top-0 z-[-1] h-full w-full object-cover blur-md"
-    type="video/mp4"
-    :src="videoStore.videoFile"
-  ></video>
+  <div class="absolute left-0 top-0 -z-20 h-full w-full dark:bg-gray-600"></div>
+
+  <Transition
+    enter-active-class="transition-opacity duration-300 ease-in-out"
+    leave-active-class="transition-opacity duration-300 ease-in-out"
+    enter-from-class="opacity-0"
+    leave-to-class="opacity-0"
+    mode="out-in"
+  >
+    <video
+      :key="videoStore.videoFile"
+      autoplay
+      muted
+      loop
+      class="absolute left-0 top-0 -z-10 h-full w-full object-cover blur-md"
+      type="video/mp4"
+      :src="videoStore.videoFile"
+    ></video>
+  </Transition>
 </template>
 
 <script lang="ts" setup>
 const videoStore = useVideoStore();
 </script>
+
+<!-- <style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style> -->
