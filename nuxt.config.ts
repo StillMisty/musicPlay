54ccb1bcf6eee22265eb3c9254eae009ecf5fd70
still <1772797842@qq.com>
2025-03-02 13:37:48 +0800

    添加元标签

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,9 +8,17 @@ export default defineNuxtConfig({
   },
 
   app: {
-    cdnURL: "./",
     head: {
-      title: "Music Play",
+      title: "爱莲说",
+      meta: [
+        {
+          charset: "utf-8",
+        },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1",
+        },
+      ],
       link: [
         {
           rel: "icon",
