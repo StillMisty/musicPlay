11e7f4be1084ed11d6d24a99342b2842a3e1c354
still <1772797842@qq.com>
2025-03-07 23:21:26 +0800

    更新依赖项版本，移除 Background.vue 中的未使用样式

--- components/Background.vue
+++ components/Background.vue
@@ -23,15 +23,3 @@
 <script lang="ts" setup>
 const videoStore = useVideoStore();
 </script>
-
-<!-- <style scoped>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.3s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-</style> -->
